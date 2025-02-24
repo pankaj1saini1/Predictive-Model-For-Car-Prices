c97c0718a4fa9f866aacac1c5f029bc822178081
pankaj1saini1 <pankaj1saini1@gmail.com>
2025-02-24 20:06:07 +0530

    Create model_training.py

---
+++ model_training.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+df2=pd.read_csv('E:/car2.csv')
+le = LabelEncoder()
+a= ['Brand','model','Transmission','Owner','FuelType']
+for col in a:
+    df2[col] = le.fit_transform(df2[col])
+    label_mapping.append(dict(zip(le.classes_, range(len(le.classes_)))))
+    # print(le.classes_,range(len(le.classes_)))
+print(label_mapping[1])
+
+# print(df2.head())
+
+y = df2['AskPrice']
+X = df2.drop(columns=['AskPrice'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# mo=LinearRegression()
+mo = RandomForestRegressor(n_estimators=100, random_state=42)
+mo.fit(X_train,y_train)
